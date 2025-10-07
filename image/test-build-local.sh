f39b8249cb53ee60b42e823ea6e75730a0cd72f2
hsw <hsw@syscall.io>
2025-10-07 15:28:28 +0900

    Use repo-snapshot image

--- image/test-build-local.sh
+++ image/test-build-local.sh
@@ -42,6 +42,7 @@ build_image() {
    --metadata-file "${BUILD_DIR}"/"$name"/metadata.json \
    --cache-to "type=registry,ref=${REGISTRY_HOST}/tmp/${name}-cache,mode=max" \
    --cache-from "type=registry,ref=${REGISTRY_HOST}/tmp/${name}-cache" \
+   --build-context repo-snapshot="docker-image://ghcr.io/hsw0/almalinux-repo-snapshot:latest" \
    --progress plain \
    "${extra_args[@]}" \
    "${SCRIPT_DIR}/${name}/" \
