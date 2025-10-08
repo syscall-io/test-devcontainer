d2fcd5449628c639d0c4ff4c97628635f66653c4
hsw0 <1043930+hsw0@users.noreply.github.com>
2025-10-08 11:21:58 +0900

    wip

--- start-apple.sh
+++ start-apple.sh
@@ -10,27 +10,37 @@ main() {
 
   ssh_port=52322
 
+
+  # FIXME: uid/gid mapping
+  if ! container volume inspect devcontainer-user-cache >& /dev/null; then
+    container volume create devcontainer-user-cache
+  fi
+  if ! container volume inspect devcontainer-user-local >& /dev/null; then
+    container volume create devcontainer-user-local
+  fi
+
   (set -x; container run \
     --cidfile "$cidfile" \
     --name devcontainer \
     --detach \
     --tty \
     --cpus 4 --memory 8G \
+    --user 0 \
     --publish "127.0.0.1:${ssh_port}:22" \
+    --mount "type=volume,src=devcontainer-user-cache,dst=/home/user/.cache" \
+    --mount "type=volume,src=devcontainer-user-local,dst=/home/user/.local" \
     --volume "${SCRIPT_DIR}/workspace:/workspace" \
-    --volume "${SCRIPT_DIR}/home/.cache:/home/user/.cache" \
-    --volume "${SCRIPT_DIR}/home/.config:/home/user/.config" \
     ghcr.io/hsw0/test-devcontainer:master \
   ;)
 
   local -r cid=$(< "$cidfile")
   echo "[*] Container ID: ${cid}"
 
-  local -r container_cidr=$(container inspect devcontainer | jq -r '.[0].networks[0].address')
+  local -r container_cidr=$(container inspect "$cid" | jq -r '.[0].networks[0].address')
   local -r container_address=${container_cidr%/*}
   echo "[*] Container address: ${container_address}"
 
-  container exec -i --user user devcontainer /bin/bash -c 'cat > ~/.ssh/authorized_keys' < "$SCRIPT_DIR"/authorized_keys
+  container exec -i --user user "$cid" /bin/bash -c 'umask 0077 && mkdir -p ~/.ssh && cat > ~/.ssh/authorized_keys' < "$SCRIPT_DIR"/authorized_keys
 
   return 0
 }
